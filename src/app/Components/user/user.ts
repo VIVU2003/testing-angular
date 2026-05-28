fda41b7a11942592947e7846ef3a7e28dd1d6dcf
VIVU2003 <vkewalramani_be22@thapar.edu>
2026-05-28 21:34:12 +0530

    changes

---
+++ src/app/Components/user/user.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import {userDetails,product,GetdetailsService} from'testing-lib'
+@Component({
+  selector: 'app-user',
+  standalone:true,
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+ constructor(private userService:GetdetailsService){}
+ getdetail=signal<product[]|undefined>(undefined)
+ ngOnInit()
+ {
+  this.userService.getDetails().subscribe((data)=>{
+    this.getdetail.set(data.products)
+  })
+ }
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+// import {GetdetailsService} from 'testing-lib';
+import { User } from './Components/user/user';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('testing-angular');
+  // constructor(private getservice:GetdetailsService){};
+  // getdata:any=signal([])
+  //  ngOnInit(): void{
+  //   this.getservice.getDetails().subscribe((data:any)=>{
+  //    this.getdata.set(data.products)
+  //   })
+  //  }
+  
+  
 }
